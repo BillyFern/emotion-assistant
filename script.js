3e1721bb0044f1518d9b2fec3bace3c3129d1642
Billy Fernando <billy22ti@mahasiswa.pcr.ac.id>
2026-06-27 17:12:01 +0700

    test the webcam

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
-console.log("Emotion Assistant Loaded!");
+const video = document.getElementById("webcam");
+
+async function setupCamera() {
+
+    try {
+
+        const stream = await navigator.mediaDevices.getUserMedia({
+            video: {
+                width: 640,
+                height: 480
+            },
+            audio: false
+        });
+
+        video.srcObject = stream;
+
+        return new Promise((resolve) => {
+
+            video.onloadedmetadata = () => {
+
+                console.log("Camera Ready!");
+
+                resolve(video);
+
+            };
+
+        });
+
+    } catch (err) {
+
+        console.error(err);
+
+        alert("Unable to access webcam.");
+
+    }
+
+}
+
+setupCamera();
